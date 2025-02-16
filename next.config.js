5aa1431f9b001a480672bb66ee21e0d54c998b30
maniadav <manish.yadav.elit@gmail.com>
2025-02-16 18:25:56 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const withPWA = require('next-pwa')({
 	skipWaiting: true,
 })
 
+const isProd = true //process.env.NODE_ENV === 'production'
+const repoName = 'next-pwa-template'
+
 module.exports = withPWA({
 	reactStrictMode: true,
+	output: 'export',
+	basePath: isProd ? `/${repoName}` : '',
+	assetPrefix: isProd ? `/${repoName}/` : '',
+	images: {
+		unoptimized: true,
+	},
 })
