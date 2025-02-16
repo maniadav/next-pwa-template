489f33f1536305e21bd83de795c037f8ed008cfc
maniadav <manish.yadav.elit@gmail.com>
2025-02-16 18:05:09 +0530

    first commit

---
+++ components/appbar.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const links = [
+	{ label: 'home', href: '/' },
+	{ label: 'blog', href: '/blog' },
+	{ label: 'github', href: '/github' },
+]
+
+const Appbar = () => {
+	const router = useRouter()
+
+	return (
+		<div className='fixed top-0 left-0 z-20 w-full bg-zinc-900 pt-safe'>
+			<header className='border-b bg-zinc-100 px-safe dark:border-zinc-800 dark:bg-zinc-900'>
+				<div className='mx-auto flex h-20 max-w-screen-md items-center justify-between px-6'>
+					<Link href='/'>
+						<h1 className='font-medium'>Next-PWA-Template</h1>
+					</Link>
+
+					<nav className='flex items-center space-x-6'>
+						<div className='hidden sm:block'>
+							<div className='flex items-center space-x-6 uppercase'>
+								{links.map(({ label, href }) => (
+									<Link
+										key={label}
+										href={href}
+										className={`text-sm ${
+											router.pathname === href
+												? 'text-fuchsia-500 dark:text-fuchsia-400 font-bold'
+												: 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
+										}`}
+									>
+										{label}
+									</Link>
+								))}
+							</div>
+						</div>
+
+						<div
+							title='Gluten Free'
+							className='h-10 w-10 rounded-full bg-zinc-200 bg-cover bg-center shadow-inner dark:bg-zinc-800'
+							style={{
+								backgroundImage: 'url(/images/user.png)',
+							}}
+						/>
+					</nav>
+				</div>
+			</header>
+		</div>
+	)
+}
+
+export default Appbar
---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import Page from '@/components/page'
+import Section from '@/components/section'
+import Link from 'next/link'
+
+const Index = () => (
+	<Page>
+		<Section>
+			<main className='flex flex-col items-center justify-center p-4 bg-white dark:bg-zinc-900'>
+				<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
+					What is a Progressive Web App (PWA)?
+				</h2>
+
+				<div className='mt-2 text-start max-w-2xl'>
+					<p className='text-zinc-600 dark:text-zinc-400'>
+						A{' '}
+						<span className='font-medium text-zinc-900 dark:text-zinc-50'>
+							Progressive Web App (PWA)
+						</span>{' '}
+						is a type of web application that leverages modern web technologies
+						to provide a native app-like experience. PWAs are fast, reliable,
+						and can work offline, making them a great choice for delivering a
+						seamless user experience.
+					</p>
+
+					<h3 className='mt-4 text-lg font-semibold text-zinc-800 dark:text-zinc-200'>
+						Benefits of PWAs:
+					</h3>
+
+					<ul className='mt-2 text-zinc-600 dark:text-zinc-400 text-left'>
+						<li>- Installable on any device like a native app</li>
+						<li>- Works offline with service workers</li>
+						<li>- Fast performance with efficient caching</li>
+						<li>- No need for app stores—just visit the website</li>
+						<li>- Secure and responsive across all devices</li>
+					</ul>
+				</div>
+				<div className='flex text-center mt-20'>
+					<Link
+						href='https://github.com/maniadav/next-pwa-template'
+						target='_blank'
+						rel='noopener noreferrer'
+						className='px-4 py-2 mt-6 text-sm font-medium text-white bg-fuchsia-600 rounded-lg shadow-md hover:bg-fuchsia-700 dark:bg-fuchsia-500 dark:hover:bg-fuchsia-600'
+					>
+						🚀 View on GitHub
+					</Link>
+				</div>
+			</main>
+		</Section>
+	</Page>
+)
+
+export default Index
